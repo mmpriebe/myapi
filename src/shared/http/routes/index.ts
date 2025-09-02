47db9b2ad4af5079d18eb8afef9580154ef2fd34
Marciano Priebe <marciano.priebe@gmail.com>
2025-09-02 11:01:42 -0300

    Add error handling middleware

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import AppError from '../../errors/AppError';
 
 const routes = Router();
 
 routes.get('/', (request, response) => {
+  throw new AppError('Acesso negado', 401);
+
   response.send({ message: 'ok' });
 });
 
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,7 +2,9 @@ import dotenv from 'dotenv';
 import Express from 'express';
 import cors from 'cors';
 
-import routes from './routes';
+import { Request, Response, NextFunction } from 'express';
+import routes from './routes/index';
+import AppError from '../errors/AppError';
 
 dotenv.config();
 
@@ -13,6 +15,22 @@ app.use(cors());
 
 app.use(routes);
 
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    console.log(error);
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
+
 app.listen(process.env.PORT, () => {
-  console.log(`Server started on port 3000!`);
+  console.log(`Server started on port ${process.env.PORT}!`);
 });
